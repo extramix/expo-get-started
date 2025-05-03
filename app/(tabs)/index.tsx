e94eed651a1ddeab1bae0369bfeaa0c0b0cf518d
ミック(Sirapat) <sirapat.jr@gmail.com>
2025-05-03 13:07:40 +0900

    add function to pick a photo and display it

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,28 @@
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViwer';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 import { StyleSheet, View } from "react-native";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View
       style={styles.container}
@@ -12,10 +30,11 @@ export default function Index() {
       <View style={styles.imageContainer}>
         <ImageViewer
           imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
         />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme='primary' label="Choose a photo" />
+        <Button onPress={() => pickImageAsync()} theme='primary' label="Choose a photo" />
         <Button label="Use this photo" />
       </View>
     </View>
